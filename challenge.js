d029a25b32d5c9210bec96d718e5e965ed9798f4
hoodfk <umaliita240@gmail.com>
2022-04-04 14:06:10 +0300

    done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,11 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+  // excersise tip thing
+  let bill = 200;
+  if (bill >=50 && bill <=300){
+      console.log (`your tip is  ${bill*0.15} and the total is ${ bill+bill*0.15}`)
+     
+  }
+  else if (bill > 300 ) {
+   console.log (`your tip is  ${bill*0.2} and the total is ${ bill+bill*0.2}`)
+  }
